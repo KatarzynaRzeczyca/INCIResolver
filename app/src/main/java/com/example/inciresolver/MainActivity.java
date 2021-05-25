f17b7413ff95ae1f1f7b437cce959ff7a9f30cc3
katarzyna <katarzynarzeczyca@gmail.com>
2021-05-25 12:57:20 +0200

    load gallery image

--- app/src/main/java/com/example/inciresolver/MainActivity.java
+++ app/src/main/java/com/example/inciresolver/MainActivity.java
@@ -15,7 +15,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.SearchView;
@@ -29,6 +31,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private DBProvider mydb;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,8 +93,7 @@ public class MainActivity extends AppCompatActivity {
     public void click(View view) {
         switch(view.getId()){
             case R.id.imageButtonGallery:
-                PopUpClass popUpClass = new PopUpClass();
-                popUpClass.showPopupWindow(view);
+                openGallery();
 //                Intent intentSearch = new Intent(MainActivity.this, ShowIngredient.class);
 //                startActivity(intentSearch);
                 break;//akcja
@@ -116,6 +120,21 @@ public class MainActivity extends AppCompatActivity {
             PopUpClass popUpClass = new PopUpClass();
             popUpClass.showPopupWindow(findViewById(R.id.main_page));
             //use the query to search your data somehow
+        }
+    }
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            System.out.println(imageUri.getPath());
+            //imageView.setImageURI(imageUri);
         }
     }
 }
